3288c22a7077e0bd4013cf94561bb8329db67f79
SC414 <>
2019-10-16 09:23:34 +0630

    Branch1 1st commit

--- app/src/main/java/com/example/mygittesting/MainActivity.kt
+++ app/src/main/java/com/example/mygittesting/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.mygittesting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"This is Branch 1",Toast.LENGTH_SHORT).show()
+
     }
 }
